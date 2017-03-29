7a62290fbde801b8096eca11eec96387a26126b4
L337Lobster <jazzycool9@gmail.com>
2017-03-29 08:48:19 -0400

    password hashing

--- grails-app/domain/cs320i/User.groovy
+++ grails-app/domain/cs320i/User.groovy
@@ -3,12 +3,15 @@
  */
 package cs320i
 
+import java.sql.Blob
+
 class User {
 
     String username
     String firstName
     String lastName
     String email
+    Blob password;
 
 
 }
--- grails-app/init/cs320i/BootStrap.groovy
+++ grails-app/init/cs320i/BootStrap.groovy
@@ -1,15 +1,19 @@
 package cs320i
 
-
+import java.sql.Blob
 class BootStrap {
 
     def init = { servletContext ->
 
         if(User.count() == 0)
         {
-            new User(username: 'SamuraiJack365', firstName: 'Jackson', lastName: 'Hofmann', email: 'jazzycool9@gmail.com').save()
-            new User(username: 'SamuraiJack', firstName: 'Jason', lastName: 'Hofmann', email: 'jazzy@gmail.com').save()
-            new User(username: 'Samurai', firstName: 'Jack', lastName: 'Hofmann', email: 'cool9@gmail.com').save()
+            def encrypt = new PasswordEncryptionService()
+            def salt = encrypt.generateSalt()
+            byte[] encryptedPassword = encrypt.getEncryptedPassword("password", salt)
+            Blob storePassword = encrypt.getBlobPassword(encryptedPassword)
+            new User(username: 'SamuraiJack365', password: storePassword, firstName: 'Jackson', lastName: 'Hofmann', email: 'jazzycool9@gmail.com').save()
+            new User(username: 'SamuraiJack',  password: storePassword, firstName: 'Jason', lastName: 'Hofmann', email: 'jazzy@gmail.com').save()
+            new User(username: 'Samurai', password: storePassword, firstName: 'Jack', lastName: 'Hofmann', email: 'cool9@gmail.com').save()
         }
     }
     def destroy = {
